8b53b5412203acd5682f21fb302d8c79dbfffce9
Jessica Kerr <jessitron@atomist.com>
2019-11-12 12:40:32 -0600

    Add push alias with Ratfish window

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -20,6 +20,15 @@ Function ListFilesWithMostRecentAtBottom {
 }
 Set-Alias ll ListFilesWithMostRecentAtBottom
 
-$MyScriptsLocation="$home\dotfiles\windows"
+$MyScriptsLocation = "$home\dotfiles\windows"
 
 . $MyScriptsLocation\get.ps1
+
+Function GitPush {
+    if (!(git push)) {
+        $pwd = (Get-Location).toString()
+        $wshell = New-Object -ComObject Wscript.Shell
+        $wshell.Popup("Your push failed in $pwd", 0, "Ratfish", 0x1)
+    }
+}
+Set-Alias push GitPush
