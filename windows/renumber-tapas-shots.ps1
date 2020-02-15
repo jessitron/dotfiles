2dafdd7bed2a8422144b797e744734de7bff798a
Jessica Kerr <jessitron@gmail.com>
2020-02-15 13:29:46 -0600

    Lying around

---
+++ windows/renumber-tapas-shots.ps1
@@ -0,0 +1,29 @@
+function NumberShots {
+    begin {
+        $i = 0;
+        $ShotPattern = "shot\(\d*\)";
+    }
+    process {
+        if ($_ -match "<!-- ${ShotPattern}.*-->") {
+            $i++;
+            $_ -replace $ShotPattern, "shot(${i})"
+        }
+        else { $_ } 
+    }
+    end {
+        Write-Information "Numbered ${i} shots";
+    }
+}
+
+$InformationPreference = "Continue"
+
+function Update-ShotNumbers {
+
+    Param(
+        $Path = ".\episode-script.md"
+    )
+
+    process {
+        (Get-Content $Path) | NumberShots | Set-Content $Path
+    }
+}
