2ba527a9b0b3c640f76948f80635e3961769732f
Jessica Kerr <jessitron@gmail.com>
2020-02-15 13:48:55 -0600

    Do not crash miserable when 'be' finds multiple

--- windows/be.ps1
+++ windows/be.ps1
@@ -18,7 +18,14 @@ function Set-Context {
         # This returns a PathInfo if it returns anything
         $repopath = Get-ChildItem -Path $codepath | Get-ChildItem -Filter $Project
 
+        if ($repopath.Length -gt 1) {
+            Write-Host "I found multiple things!"
+            Write-Output ($repopath | Format-List -Property FullName)
+            return
+        }
+
         if ($repopath) {
+            Write-Host $repopath.GetType()
             Write-Host "Found something"
             Set-Location -Path $repopath.FullName # You have to pass a string
             # I could also do $repopath | Set-Location but that changes my prompt to something weird
