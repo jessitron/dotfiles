4ae10c2dc5e5cb470ebc492007895958bcf5b05c
Jessica Kerr <jessitron@gmail.com>
2020-10-19 11:39:09 -0500

    vscode settings something

--- windows/bash-equivalents.ps1
+++ windows/bash-equivalents.ps1
@@ -5,3 +5,9 @@ function TeachAbout-Touch {
     new-item $args
 }
 Set-Alias -name touch -Value TeachAbout-Touch
+
+function Print-FirstLines { 
+    Write-Host "hey, the powershell is cat <file> | select -first 10"
+    cat $args | select -first 10
+}
+Set-Alias -name head -Value Print-FirstLines
