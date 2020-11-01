8d4a814267a49ff959b9b151bbb661d7860b55df
Jessica Kerr <jessitron@gmail.com>
2020-11-01 11:29:09 -0600

    Remove old atomist stuff

--- aliases.sh
+++ aliases.sh
@@ -6,9 +6,3 @@ alias gs='git status'
 alias gp='git push || echo "DAMMIT"'
 
 alias c='git add . && git commit -m rebaseme'
-
-
-alias asln='atomist start --local --no-compile'
-alias aalr='atomist analyze local repositories --localDirectory=$(pwd)'
-
-export ATOMIST_PROJECTS=$HOME/code
