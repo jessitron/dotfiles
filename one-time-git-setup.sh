f13dda877ada9371e12de3b394286e28985a71db
Jessica Kerr <jessitron@gmail.com>
2024-04-22 14:20:11 -0500

    alias 'git jsdiff'

--- one-time-git-setup.sh
+++ one-time-git-setup.sh
@@ -4,6 +4,7 @@ git config --global alias.ff 'merge --ff-only'
 git config --global alias.fix 'commit --amend --no-edit'
 git config --global alias.home 'rev-parse --show-toplevel'
 git config --global alias.note 'commit --allow-empty -m'
+git config --global alias.jsdiff 'diff -w -- . ":(exclude)**/package-lock.json"'
 
 git config --global user.name "Jessica Kerr"
 git config --global user.email "jessitron@gmail.com"
